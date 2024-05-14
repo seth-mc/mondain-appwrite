44ef263b06fe2c66275f3a466d8b09c085594503
Seth McClimans <sethmcclimans@Seths-Air.hitronhub.home>
2024-05-13 22:17:47 -0400

    changing playlist

--- src/_root/pages/Dj.tsx
+++ src/_root/pages/Dj.tsx
@@ -184,8 +184,7 @@ const Dj = () => {
     </div>
       )}
       <PlayerComponent></PlayerComponent>
-      <iframe title="frame01" className="sc-widget" frameBorder="no" allow="autoplay"  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1533531358&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
-    
+      <iframe title="frame01" className="sc-widget" frameBorder="no" allow="autoplay"  src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1822931166&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
     </div>
   );
 }
